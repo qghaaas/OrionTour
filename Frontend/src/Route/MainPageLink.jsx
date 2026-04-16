529bd3111b30c9bf66a379edf6e28fd40be4ec60
xavcramer <artiomkoz712@gmail.com>
2026-04-16 17:33:03 +0200

    authback

--- Frontend/src/Route/MainPageLink.jsx
+++ Frontend/src/Route/MainPageLink.jsx
@@ -1,10 +1,13 @@
 import Header from "../MainPage/Header/Header"
-
+import Regtest from "../MainPage/Regtest/Regtest"
+import Logtest from "../MainPage/Logtest/Logtest"
 
 export default function MainPageLink() {
     return (
         <>
             <Header />
+            <Regtest />
+            <Logtest />
         </>
     )
 }
