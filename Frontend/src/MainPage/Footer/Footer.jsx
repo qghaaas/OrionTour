79262c75353b399ea97f191bfe503a7308e95905
qghaaas <kapitanov.2006@mail.ru>
2026-04-19 19:44:37 +0200

    footer

---
+++ Frontend/src/MainPage/Footer/Footer.jsx
@@ -0,0 +1,56 @@
+import '../../main.css'
+import './Footer.css'
+import { Link } from 'react-router-dom'
+import logo from '../../mainIMG/logo.svg'
+import telegram from './img/telegram.svg'
+import vk from './img/vk.svg'
+import location from './img/location.svg'
+import phone from './img/Phone.svg'
+import atsign from './img/Atsign.svg'
+
+
+
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <div className="container">
+                    <div className="footer-inner">
+                        <Link className='' to="#"><img src={logo} alt="" /></Link>
+
+                        <ul className='footer-item'>
+                            <li><Link to="#">Главная страница</Link></li>
+                            <li><Link to="#">Направления</Link></li>
+                            <li><Link to="#">Поиск тура</Link></li>
+                        </ul>
+
+                        <ul className='footer-item'>
+                            <li><Link to="#">Внутренний туризм</Link></li>
+                            <li><Link to="#">Направления</Link></li>
+                            <li><Link to="#">Блог</Link></li>
+                        </ul>
+
+                        <ul className='footer-item'>
+                            <li><Link to="#">Контакты</Link></li>
+                            <li><Link to="#">О нас</Link></li>
+                        </ul>
+
+                        <ul className='footer-item footer-item_sep'>
+                            <li><img src={location} alt="" /><Link to="#"> orion_tour_39</Link></li>
+                            <li><img src={phone} alt="" /><Link to="#">+7 (4012) 75-95-99</Link></li>
+                            <li><img src={atsign} alt="" /><Link to="#">Калининград,площадь Победы, 4</Link></li>
+                        </ul>
+
+                        <div className='social-link'>
+                            <p>Соц.сети</p>
+                            <div>
+                                <Link><img src={telegram} alt="" /></Link>
+                                <Link><img src={vk} alt="" /></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
