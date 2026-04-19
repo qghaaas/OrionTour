b8288d2da1f77ff4dfb8e6f7722722815dc181f8
xavcramer <artiomkoz712@gmail.com>
2026-04-19 16:53:59 +0200

    idk

--- Frontend/src/MainPage/Header/Header.jsx
+++ Frontend/src/MainPage/Header/Header.jsx
@@ -2,49 +2,46 @@ import '../../main.css'
 import './Header.css'
 import logo from '../../mainIMG/logo.svg'
 import { Link } from 'react-router-dom'
-import { useState } from "react"
-import AuthModal from '../../User/Auth/AuthModal'
-
 
 
 export default function Header() {
-    const [isAuthOpen, setIsAuthOpen] = useState(false);
-
     return (
         <>
             <header>
                 <div className="container">
                     <div className="header-inner">
-                        <Link className='logo-link' to="#">
-                            <img src={logo} alt="" />
-                        </Link>
+                        <Link className='logo-link' to="#"> <img src={logo} alt="" /></Link>
 
                         <nav className="header-nav">
                             <ul className='header-menu'>
-                                <li><Link to="#">Направления</Link></li>
-                                <li><Link to="#">Поиск тура</Link></li>
-                                <li><Link to="#">Внутренний туризм</Link></li>
-                                <li><Link to="#">Блог</Link></li>
-                                <li><Link to="#">Конструктор</Link></li>
-                                <li><Link to="#">О нас</Link></li>
-                                <li><Link to="#">Контакты</Link></li>
+                                <li>
+                                    <Link to="#">Направления</Link>
+                                </li>
+                                <li>
+                                    <Link to="#">Поиск тура</Link>
+                                </li>
+                                <li>
+                                    <Link to="#">Внутренний туризм</Link>
+                                </li>
+                                <li>
+                                    <Link to="#">Блог</Link>
+                                </li>
+                                <li>
+                                    <Link to="#">Конструктор</Link>
+                                </li>
+                                <li>
+                                    <Link to="#">О нас</Link>
+                                </li>
+                                <li>
+                                    <Link to="#">Контакты</Link>
+                                </li>
                             </ul>
                         </nav>
 
-                        <button
-                            className="login-link main-btn_site"
-                            onClick={() => setIsAuthOpen(true)}
-                        >
-                            Войти
-                        </button>
+                        <Link className='login-link' to="#">Войти</Link>
                     </div>
                 </div>
             </header>
-
-            <AuthModal
-                isOpen={isAuthOpen}
-                onClose={() => setIsAuthOpen(false)}
-            />
         </>
-    );
+    )
 }
